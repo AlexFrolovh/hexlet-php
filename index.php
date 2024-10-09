186121a2ee39a6d5a42acc7ea5033640c799088d
alex <alexfrolovyh@list.ru>
2024-10-09 17:09:18 +0300

    update

--- index.php
+++ index.php
@@ -2,9 +2,6 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-// Файл не включается напрямую
-// Он загрузится автоматически благодаря автозагрузке
 use Hexlet\Php\Runner;
 
 print_r(Runner\run());
-
